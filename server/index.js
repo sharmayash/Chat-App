212699c426f1d44a5959f971181cd02a4cbf26d7
yash <sharmayash44ys@gmail.com>
2020-11-17 02:34:02 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -1,33 +1,37 @@
-const cors = require("cors")
-const path = require("path")
-const express = require("express")
-const passport = require("passport")
-const schema = require("./graphql/index")
-const expressGraphql = require("express-graphql")
+const cors = require("cors");
+const express = require("express");
+const passport = require("passport");
+const schema = require("./graphql/index");
+const expressGraphql = require("express-graphql");
 
-const app = express()
-const port = process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000;
 
 // --------- Middlewares -----------
 
 // 1 Allow request for client from different port and url
 
-app.use(cors())
+app.use(cors());
 
 // 2. db config
 
-require("./dbConfig/")
+require("./dbConfig/");
 
 // 3. passport
 
-app.use(passport.initialize())
-require("./config/passport")(passport)
+app.use(passport.initialize());
+require("./config/passport")(passport);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"))
+  const root = require("path").join(__dirname, "../client", "build");
+  app.use(express.static(root));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"))
-  })
+    res.sendFile("index.html", { root });
+  });
+  // app.use(express.static("../client/build"));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  // });
 }
 
 // 4. Graphql
@@ -38,16 +42,16 @@ app.use(
     schema,
     graphiql: true,
   })
-)
+);
 
 // -------- Middlewares end ---------
 
 // listening to server
 
-const server = app.listen(port, () => console.log(`server started on ${port}`))
+const server = app.listen(port, () => console.log(`server started on ${port}`));
 
 // for socket related tasks
 
-const io = require("socket.io").listen(server)
+const io = require("socket.io").listen(server);
 
-require("./ioServer")(io)
+require("./ioServer")(io);
